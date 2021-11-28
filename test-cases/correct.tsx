b679ebcd7d6018cc0d381b8454a5fd4697be0032
David Madner <david@tourradar.com>
2021-11-28 17:40:11 +0100

    fix: differentiate between type and normal imports

--- test-cases/correct.tsx
+++ test-cases/correct.tsx
@@ -1,8 +1,11 @@
+import * as React from 'react';
+import type { FC } from 'react';
+
 import lodash from 'lodash';
 
 import { testFunction } from './correct';
 
-export const TestComponent = () => {
+export const TestComponent: FC<{ children: React.ReactNode }> = () => {
   return (
     <h1>
       {lodash.pick([], 1)} + {testFunction()}
